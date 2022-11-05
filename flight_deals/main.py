9caaedc98a3f34e19e16b590eb2aecff32634abb
Igor Ferreira <iferreira.n01@gmail.com>
2022-11-05 10:19:54 -0300

    Create the files separated by functionality

---
+++ flight_deals/main.py
@@ -0,0 +1,8 @@
+# This file will need to use the DataManager, FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
