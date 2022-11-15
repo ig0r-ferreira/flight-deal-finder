815f5caab02d601c5b314f7510cc383e0584158c
Igor Ferreira <iferreira.n01@gmail.com>
2022-11-14 21:56:55 -0300

    Create a method to search flights

--- flight_deals/flight_search.py
+++ flight_deals/flight_search.py
@@ -1,11 +1,33 @@
 from typing import Any
+from urllib.parse import urljoin
 
 import requests
-from pydantic import HttpUrl, SecretStr, validate_arguments
+from pydantic import (
+    BaseModel,
+    HttpUrl,
+    PositiveInt,
+    SecretStr,
+    validate_arguments,
+)
+
+
+class FlightSearchParams(BaseModel):
+    fly_from: str
+    fly_to: str
+    date_from: str
+    date_to: str
+    curr: str
+    nights_in_dst_from: PositiveInt = 7
+    nights_in_dst_to: PositiveInt = 14
+    flight_type: str = 'round'
+    price_from: PositiveInt | None = None
+    price_to: PositiveInt | None = None
+    limit: PositiveInt = 100
 
 
 class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
+    """This class is responsible for talking to the Flight Search API."""
+
     @validate_arguments
     def __init__(self, base_url: HttpUrl, api_key: SecretStr) -> None:
         self.base_url = base_url
@@ -13,7 +35,10 @@ class FlightSearch:
 
     @property
     def headers(self) -> dict[str, Any]:
-        return {'apikey': self.api_key.get_secret_value()}
+        return {
+            'apikey': self.api_key.get_secret_value(),
+            'Content-Type': 'application/json',
+        }
 
     @validate_arguments
     def get_iata_code_by_city_name(self, city_name: str) -> str:
@@ -23,7 +48,7 @@ class FlightSearch:
             'limit': 1,
         }
         response = requests.get(
-            url=f'{self.base_url}locations/query',
+            url=urljoin(self.base_url, 'locations/query'),
             headers=self.headers,
             params=params,
         )
@@ -37,3 +62,18 @@ class FlightSearch:
             return ''
 
         return locations[0].get('code', '')
+
+    @validate_arguments
+    def search_flights(
+        self, flight_params: FlightSearchParams
+    ) -> dict[str, Any]:
+        response = requests.get(
+            url=urljoin(self.base_url, 'v2/search'),
+            headers=self.headers,
+            params=flight_params.dict(),
+        )
+        response.raise_for_status()
+
+        data = response.json().get('data', [])
+
+        return data
--- tests/test_flight_search.py
+++ tests/test_flight_search.py
@@ -1,18 +1,23 @@
+from datetime import date, timedelta
+from urllib.parse import urljoin
+
 import pytest
 from pydantic import HttpUrl, SecretStr, parse_obj_as
 from requests_mock import Mocker
 
-from flight_deals.flight_search import FlightSearch
-
-TEST_URL: str = 'https://flightapi.com/'
-TEST_API_KEY: str = '12345'
+from flight_deals.flight_search import FlightSearch, FlightSearchParams
+from flight_deals.settings import get_settings
 
 
 @pytest.fixture
 def flight_search() -> FlightSearch:
-    return FlightSearch(
-        parse_obj_as(HttpUrl, TEST_URL), SecretStr(TEST_API_KEY)
+    settings = get_settings()
+    settings.FLIGHT_API.BASE_URL = parse_obj_as(
+        HttpUrl, 'https://flightapi.com/'
     )
+    settings.FLIGHT_API.KEY = parse_obj_as(SecretStr, '12345')
+
+    return FlightSearch(settings.FLIGHT_API.BASE_URL, settings.FLIGHT_API.KEY)
 
 
 def test_iata_code_is_par_when_city_name_is_paris(
@@ -21,7 +26,7 @@ def test_iata_code_is_par_when_city_name_is_paris(
     city_name = 'Paris'
 
     requests_mock.get(
-        url=f'{flight_search.base_url}locations/query',
+        url=urljoin(flight_search.base_url, 'locations/query'),
         headers=flight_search.headers,
         json={'locations': [{'name': city_name, 'code': 'PAR'}]},
     )
@@ -34,10 +39,34 @@ def test_iata_code_is_empty_str_when_the_city_is_not_found(
     flight_search: FlightSearch, requests_mock: Mocker
 ) -> None:
     requests_mock.get(
-        url=f'{flight_search.base_url}locations/query',
+        url=urljoin(flight_search.base_url, 'locations/query'),
         headers=flight_search.headers,
         json={'locations': []},
     )
     iata_code = flight_search.get_iata_code_by_city_name('xyz')
 
     assert iata_code == ''
+
+
+def test_search_flights_must_return_a_list(
+    flight_search: FlightSearch,
+    requests_mock: Mocker,
+) -> None:
+
+    search_params = FlightSearchParams(
+        fly_from='SSA',
+        fly_to='NYC',
+        date_from=f'{date.today() + timedelta(days=1):%d/%m/%Y}',
+        date_to=f'{date.today() + timedelta(days=180):%d/%m/%Y}',
+        curr='BRL',
+        price_to=10_000,
+    )
+
+    requests_mock.get(
+        url=urljoin(flight_search.base_url, 'v2/search'),
+        headers=flight_search.headers,
+        json={'data': [{'id': '123456'}]},
+    )
+    result = flight_search.search_flights(search_params)
+
+    assert isinstance(result, list)
