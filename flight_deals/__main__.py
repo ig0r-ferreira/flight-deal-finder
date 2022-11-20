afde78f00b3750b27531fa478b9af4066bc4ebbe
Igor Ferreira <iferreira.n01@gmail.com>
2022-11-19 22:12:40 -0300

    Add cheapest flights notification

--- flight_deals/__main__.py
+++ flight_deals/__main__.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta
+from smtplib import SMTP
 
 from flight_deals.data_manager import DataManager
+from flight_deals.email_client import EmailClient, make_message
 from flight_deals.flight_data import FlightItinerary
 from flight_deals.flight_search import FlightSearch, FlightSearchParams
 from flight_deals.settings import get_settings
@@ -17,6 +19,15 @@ def main() -> None:
         base_url=settings.FLIGHT_API.BASE_URL,
         api_key=settings.FLIGHT_API.KEY,
     )
+    email_client = EmailClient(
+        smtp_server=SMTP(
+            host=f'{settings.SMTP_SERVER.HOST}:{settings.SMTP_SERVER.PORT}'
+        ),
+        credentials=(
+            settings.SMTP_SERVER.USERNAME.get_secret_value(),
+            settings.SMTP_SERVER.PASSWORD.get_secret_value(),
+        ),
+    )
 
     sheet_name = 'prices'
     destinations = data_manager.get_rows_from_sheet(sheet_name)
@@ -24,7 +35,6 @@ def main() -> None:
     tomorrow = f'{date.today() + timedelta(days=1):%d/%m/%Y}'
     six_months_from_now = f'{date.today() + timedelta(days=180):%d/%m/%Y}'
 
-    flights_to_notify = []
     for destination in destinations:
 
         destination_code = flight_search.get_iata_code_by_city_name(
@@ -47,11 +57,19 @@ def main() -> None:
         if not available_itineraries:
             continue
 
-        flights_to_notify.append(
-            FlightItinerary.parse_obj(available_itineraries[0])
-        )
+        flight = FlightItinerary.parse_obj(available_itineraries[0])
 
-    print(flights_to_notify)
+        email_client.send_message(
+            make_message(
+                from_address=settings.EMAIL.SENDER,
+                to_address=settings.EMAIL.RECIPIENTS,
+                subject=(
+                    'Low price alert! Flight from '
+                    f'{flight.departure_city} to {flight.destination_city}'
+                ),
+                body=str(flight),
+            )
+        )
 
 
 if __name__ == '__main__':
