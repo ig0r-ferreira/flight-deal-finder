2aedeb6b14f501146ded85b80b976afb847612d2
Igor Ferreira <iferreira.n01@gmail.com>
2022-11-26 15:14:45 -0300

    Refactor data manager tests to be sheet api tests

--- tests/test_sheet_api.py
+++ tests/test_sheet_api.py
@@ -13,101 +13,101 @@ from flight_deals.sheet_api import SheetAPI
 TEST_URL: str = 'https://myapi.com/projects/mysheet/'
 TEST_AUTH: str = 'Bearer 123456'
 TEST_SHEET_NAME = 'example'
-DataManagerFactory = Callable[[str, DefaultArg(str | None, None)], SheetAPI]
+SheetAPIFactory = Callable[[str, DefaultArg(str | None, None)], SheetAPI]
 
 
 @pytest.fixture
-def make_data_manager() -> DataManagerFactory:
-    def _make_data_manager(url: str, auth: str | None = None) -> SheetAPI:
+def make_sheet_api() -> SheetAPIFactory:
+    def _make_sheet_api(url: str, auth: str | None = None) -> SheetAPI:
         return SheetAPI(
             spreadsheet_url=parse_obj_as(HttpUrl, url),
             auth=SecretStr(auth) if auth is not None else auth,
         )
 
-    return _make_data_manager
+    return _make_sheet_api
 
 
-def test_data_manager_headers_are_empty_when_started_without_authorization(
-    make_data_manager: DataManagerFactory,
+def test_sheet_api_headers_are_empty_when_started_without_authorization(
+    make_sheet_api: SheetAPIFactory,
 ) -> None:
-    data_manager = make_data_manager(TEST_URL)
+    sheet_api = make_sheet_api(TEST_URL)
 
-    assert not data_manager.headers
+    assert not sheet_api.headers
 
 
 def test_get_rows_from_sheet_with_invalid_auth_key(
-    make_data_manager: DataManagerFactory, requests_mock: Mocker
+    make_sheet_api: SheetAPIFactory, requests_mock: Mocker
 ) -> None:
 
-    data_manager = make_data_manager(TEST_URL, 'invalid_auth')
+    sheet_api = make_sheet_api(TEST_URL, 'invalid_auth')
 
     requests_mock.get(
-        url=urljoin(data_manager.spreadsheet_url, TEST_SHEET_NAME),
-        headers=data_manager.headers,
+        url=urljoin(sheet_api.spreadsheet_url, TEST_SHEET_NAME),
+        headers=sheet_api.headers,
         status_code=401,
     )
 
     with pytest.raises(HTTPError) as exc_info:
-        data_manager.get_rows_from_sheet(TEST_SHEET_NAME)
+        sheet_api.get_rows_from_sheet(TEST_SHEET_NAME)
 
     assert exc_info.match('401 Client Error')
 
 
 def test_get_rows_from_sheet_to_a_non_existent_sheet(
-    make_data_manager: DataManagerFactory, requests_mock: Mocker
+    make_sheet_api: SheetAPIFactory, requests_mock: Mocker
 ) -> None:
 
-    data_manager = make_data_manager(TEST_URL, TEST_AUTH)
+    sheet_api = make_sheet_api(TEST_URL, TEST_AUTH)
     sheet_name = 'non-existent-sheet'
 
     requests_mock.get(
-        url=urljoin(data_manager.spreadsheet_url, sheet_name),
-        headers=data_manager.headers,
+        url=urljoin(sheet_api.spreadsheet_url, sheet_name),
+        headers=sheet_api.headers,
         status_code=404,
     )
 
     with pytest.raises(HTTPError) as exc_info:
-        data_manager.get_rows_from_sheet(sheet_name)
+        sheet_api.get_rows_from_sheet(sheet_name)
 
     assert exc_info.match('404 Client Error')
 
 
 def test_get_rows_from_sheet_must_return_a_list(
-    make_data_manager: DataManagerFactory, requests_mock: Mocker
+    make_sheet_api: SheetAPIFactory, requests_mock: Mocker
 ) -> None:
 
-    data_manager = make_data_manager(TEST_URL, TEST_AUTH)
+    sheet_api = make_sheet_api(TEST_URL, TEST_AUTH)
 
     requests_mock.get(
-        url=urljoin(data_manager.spreadsheet_url, TEST_SHEET_NAME),
-        headers=data_manager.headers,
+        url=urljoin(sheet_api.spreadsheet_url, TEST_SHEET_NAME),
+        headers=sheet_api.headers,
         status_code=200,
         json={TEST_SHEET_NAME: [{'key1': 25.50}, {'key2': 30.00}]},
     )
 
-    rows = data_manager.get_rows_from_sheet(TEST_SHEET_NAME)
+    rows = sheet_api.get_rows_from_sheet(TEST_SHEET_NAME)
 
     assert isinstance(rows, list)
 
 
 def test_update_sheet_row_successfully(
-    make_data_manager: DataManagerFactory, requests_mock: Mocker
+    make_sheet_api: SheetAPIFactory, requests_mock: Mocker
 ) -> None:
 
-    data_manager = make_data_manager(TEST_URL, TEST_AUTH)
+    sheet_api = make_sheet_api(TEST_URL, TEST_AUTH)
     row_id = 2
     body = {'example': {'city': 'x', 'iataCode': 'y', 'lowestPrice': 50}}
 
     requests_mock.put(
         url=urljoin(
-            data_manager.spreadsheet_url,
+            sheet_api.spreadsheet_url,
             posixpath.join(TEST_SHEET_NAME, str(row_id)),
         ),
-        headers=data_manager.headers,
+        headers=sheet_api.headers,
         status_code=200,
         json=body,
     )
 
-    result = data_manager.update_sheet_row(TEST_SHEET_NAME, row_id, body)
+    result = sheet_api.update_sheet_row(TEST_SHEET_NAME, row_id, body)
 
     assert result == body
